596fa913368d9ac66fb6f5234a6e9f6be24984b3
zunh4 <zunh40125@gmail.com>
2024-12-20 17:25:16 +0900

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,8 @@
+// 클릭 가능한 영역 요소 가져오기
+const clickableArea = document.getElementById('target');
+
+// 클릭 이벤트 추가
+clickableArea.addEventListener('click', () => {
+    // 페이지 이동
+    window.location.href = 'index6.html';
+});
